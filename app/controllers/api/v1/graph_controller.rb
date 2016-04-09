3525e6a01779eccc005b6fa62f3c14cedb1f2b2b
Chris Underwood <cunderwoodmn@gmail.com>
2016-04-09 15:46:18 -0700

    add error handling into the show action in the graph controller

--- app/controllers/api/v1/graph_controller.rb
+++ app/controllers/api/v1/graph_controller.rb
@@ -8,9 +8,14 @@ class Api::V1::GraphController < ApplicationController
   def show
     cat = params[:category] || 'sports'
     @category = Category.where(:cat_title => cat.capitalize).first
-    @links = Link.where(:cl_to => @category.cat_title, :cl_type => "subcat")
+    if @category
+      @links = Link.where(:cl_to => @category.cat_title,
+                          :cl_type => "subcat")
 
-    render :json => @links, each_serializer: LinkSerializer, root: @category.cat_title.downcase
+      render :json => @links, each_serializer: LinkSerializer, root: @category.cat_title.downcase
+    else
+      render :json =>{:error => {:text => "404 Not Found", :status => 404}}
+    end  
   end
 
   private
